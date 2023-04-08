66432623565d4e5c97c80b3820d48b0d529caf99
Ivan Nikolaenko <nikolaenkostudio@gmail.com>
2023-04-08 13:32:10 +0300

    first commit of task

---
+++ src/js/gamesavingloader.js
@@ -0,0 +1,23 @@
+import GameSaving from "./gamesaving";
+import json from "./parser";
+import read from "./reader";
+
+class GameSavingLoader {
+  constructor() {
+    this.read = read();
+  }
+
+  async load() {
+    try {
+      const buffer = await this.read;
+      const arr = await json(buffer);
+      const result = JSON.parse(arr);
+      const gameSavingResult = new GameSaving(result);
+      return gameSavingResult;
+    } catch (e) {
+      console.error(e);
+    }
+  }
+}
+
+export default GameSavingLoader;
